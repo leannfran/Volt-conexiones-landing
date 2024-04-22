4aa8f1672a62bc9d62b28240dbbc2b4728dd7128
Juan Miguel Spolzino Mendoza <juan.spolisino@gmail.com>
2024-04-22 12:44:44 -0400

    Agregando Hero

--- volt/app/page.js
+++ volt/app/page.js
@@ -1,27 +1,9 @@
+import Hero from "./components/Hero.jsx"
 
 export default function Home() {
   return (
     <>
-      <section className="text-gray-700 body-font">
-        <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
-          <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-            <h1 className="title-font sm:text-4xl text-3xl mb-4 font-bold text-gray-900">Soluciones Eléctricas Personalizadas
-              <br className="hidden lg:inline-block" />antes de que necesites encender la luz
-            </h1>
-            <p className="mb-8 leading-relaxed">
-              Entendemos que cada espacio es único. Por eso, adaptamos nuestras soluciones a las necesidades específicas de tu negocio, asegurando un servicio eficiente y de máxima calidad.
-            </p>
-            <div className="flex justify-center">
-              <button className="inline-flex text-white bg-blue-500 border-0 py-2 px-6 focus:outline-none hover:bg-blue-600 rounded text-lg transition duration-300 ease-in-out transform hover:-translate-y-1">Descubre Más</button>
-              <button className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg transition duration-300 ease-in-out transform hover:-translate-y-1">Contáctanos</button>
-            </div>
-          </div>
-          <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-            <img className="object-cover object-center rounded" alt="hero" src="https://dummyimage.com/720x600/F3F4F7/8693ac" />
-          </div>
-        </div>
-      </section>
-
+      <Hero />
       <section className="text-gray-600 body-font">
         <div className="container px-5 py-24 mx-auto">
           <div className="flex flex-wrap -m-4 text-center">
