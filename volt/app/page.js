3e9c9b6c785d50846317b54a24863a33c1925a1b
Juan Miguel Spolzino Mendoza <juan.spolisino@gmail.com>
2024-04-22 15:20:10 -0400

    Agregando OurServices

--- volt/app/page.js
+++ volt/app/page.js
@@ -1,42 +1,13 @@
 import Hero from "./components/Hero.jsx"
 import Statistic from "./components/Statistic.jsx"
+import OurServices from "./components/OurServices.jsx"
 
 export default function Home() {
   return (
     <>
       <Hero />
       <Statistic />
-      <section className="text-gray-600 body-font">
-        <div className="container px-5 py-24 mx-auto">
-          <div className="text-center mb-20">
-            <h1 className="sm:text-3xl text-2xl font-medium title-font text-gray-900 mb-4">Nuestros Servicios</h1>
-            <p className="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto text-gray-500s">Explora la variedad de servicios eléctricos que ofrecemos, diseñados para garantizar la eficiencia y seguridad de tus instalaciones.</p>
-          </div>
-          <div className="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4">
-            <div className="p-4 md:w-1/3 sm:mb-0 mb-6">
-              <div className="rounded-lg h-64 overflow-hidden">
-                <img alt="content" className="object-cover object-center h-full w-full" src="https://dummyimage.com/1200x500" />
-              </div>
-              <h2 className="text-xl font-medium title-font text-gray-900 mt-5">Instalación y Mantenimiento</h2>
-              <p className="text-base leading-relaxed mt-2">Servicios completos de instalación y mantenimiento para asegurar el máximo rendimiento y seguridad de tus sistemas eléctricos.</p>
-            </div>
-            <div className="p-4 md:w-1/3 sm:mb-0 mb-6">
-              <div className="rounded-lg h-64 overflow-hidden">
-                <img alt="content" className="object-cover object-center h-full w-full" src="https://dummyimage.com/1200x500" />
-              </div>
-              <h2 className="text-xl font-medium title-font text-gray-900 mt-5">Auditorías Energéticas</h2>
-              <p className="text-base leading-relaxed mt-2">Evalúa la eficiencia de tu consumo eléctrico y obtén recomendaciones personalizadas para reducir costos y mejorar la sostenibilidad.</p>
-            </div>
-            <div className="p-4 md:w-1/3 sm:mb-0 mb-6">
-              <div className="rounded-lg h-64 overflow-hidden">
-                <img alt="content" className="object-cover object-center h-full w-full" src="https://dummyimage.com/1200x500" />
-              </div>
-              <h2 className="text-xl font-medium title-font text-gray-900 mt-5">Soluciones de Automatización</h2>
-              <p className="text-base leading-relaxed mt-2">Integra sistemas de automatización avanzados para mejorar la eficiencia y controlar tus instalaciones eléctricas con la última tecnología.</p>
-            </div>
-          </div>
-        </div>
-      </section>
+      <OurServices />
 
       <section className="text-gray-600 body-font">
         <div className="container px-5 pt-24 pb-5 mx-auto">
