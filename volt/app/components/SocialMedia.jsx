7cfe3f387edb8f03bf4637a668f6bc06b607c330
Juan Miguel Spolzino Mendoza <juan.spolisino@gmail.com>
2024-04-22 20:56:41 -0400

    Creando SocialMedia Component

---
+++ volt/app/components/SocialMedia.jsx
@@ -0,0 +1,37 @@
+"use client";
+import { TfiInstagram } from "react-icons/tfi";
+import { SiWhatsapp } from "react-icons/si";
+
+export default function SocialMedia() {
+  return (
+    <section>
+      <div className="container mx-auto px-5 py-24">
+        <div className="text-center mb-20">
+          <h1 className="sm:text-3xl text-2xl font-bold title-font text-gray-400 mb-4">
+            Síguenos en Redes Sociales
+          </h1>
+          <p className="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto text-gray-500">
+            Mantente actualizado con las últimas noticias y proyectos de Volt
+            Conexiones siguiéndonos en nuestras redes sociales.
+          </p>
+        </div>
+        <div className="flex justify-center">
+          <a
+            href="https://instagram.com/@"
+            target="_blank"
+            class="m-4 btn-glass text-white border-0 p-2 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+          >
+            <TfiInstagram />
+          </a>
+          <a
+            href="https://wa.link/#"
+            target="_blank"
+            class="m-4 btn-glass text-white border-0 p-2 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+          >
+            <SiWhatsapp />
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+}
